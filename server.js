ea9680eef4b67d9d193a704fe369cc772be66878
DPGoertzen <dpgoertzen@gmail.com>
2016-08-13 17:53:59 -0500

    maybe this does it

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var passport = require('passport'); // npm install
 var session = require('express-session'); // npm install
 var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
-var User = require('./models/user.js');
+var User = require('./models/User.js');
 var login = require('./routes/login.js');
 var register = require('./routes/register.js');
 var index = require('./routes/index.js');
-var book = require('./routes/book.js');
+var book = require('./routes/Book.js');
 
 var mongoURI;
 
